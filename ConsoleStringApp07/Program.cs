92d4d6e0652ab876e2715592fc08473893bde4ab
ciminoce <ciminoce@yahoo.com.ar>
2021-08-23 20:47:36 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleStringApp07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStringApp07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nroCaracteres = 10;
+            StringBuilder password = new StringBuilder();
+            Random r = new Random();
+            for (int i = 0; i < nroCaracteres; i++)
+            {
+                char c =(char) r.Next(33, 126);
+                password.Append(c);
+            }
+
+            Console.WriteLine(password.ToString());
+            Console.ReadLine();
+        }
+    }
+}
